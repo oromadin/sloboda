d1e74b75077b88a1b7c81834a5f7d18652c55615
oromadin <osromadin@gmail.com>
2016-09-06 10:33:48 +0300

    Update xerox.rb

--- xerox.rb
+++ xerox.rb
@@ -1,3 +1,12 @@
+#-------------------------------------------------------------------------------
+#Obviously, first copy we have to do at fastest Xerox, so we need to find out
+#min(x,y). In what follows, we will consider the fastest one with operation time
+#x, its means that time counter increases by x sec and counter n is decreased by
+#one. The fastest Xerox will be working all time t, second one - (t - x) time, so 
+#fastest Xerox will make t/x copies, second one - (t - x)/y copies.
+#Thus our solution reduces to the inequality t/x + (t - x)/y <= n - 1.                  
+#-------------------------------------------------------------------------------
+
 def time(n, x, y)
   if x > y 
     x, y = y, x
@@ -8,6 +17,8 @@ def time(n, x, y)
   end
 t
 end
+
+#-------------------------------------------------------------------------------
 
 n = 6
 x = 3
