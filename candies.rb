dbaf93b6de0b67669c9950476f5f5a5d05e8db81
oromadin <osromadin@gmail.com>
2016-09-06 10:33:11 +0300

    Update candies.rb

--- candies.rb
+++ candies.rb
@@ -1,9 +1,16 @@
-def count(x, y, z, w)
+#-------------------------------------------------------------------------------
+#Solution is reduced to search integers coefficients in equation 
+#                   w = k1*x + k2*y + k3*z 
+#For that we iterate over coefficients k1,k2,k3 in cycle and each time, when
+#the equality is satisfied, increase variants counter. 
+#-------------------------------------------------------------------------------
+
+def count(x, y, z, w)          
 n = 0
-    for k1 in 0..w/x
+    for k1 in 0..w/x                        
         for k2 in 0..w/y
             for k3 in 0..w/z
-                if w === k1*x + k2*y + k3*z
+                if w === k1*x + k2*y + k3*z     
                     n += 1
                 end
             end
@@ -12,8 +19,10 @@ n = 0
 n
 end
 
-x = 200
-y = 250
+#-------------------------------------------------------------------------------
+
+x = 10
+y = 25
 z = 15
 w = 40
 puts count(x, y, z, w)
